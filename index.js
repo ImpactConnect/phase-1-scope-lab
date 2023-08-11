116c365d3a843004a569fbf3382967a5adcae8ed
Omotayo <kehindeomotayo11@gmail.com>
2023-08-11 20:29:04 +0100

    Done With This

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+let customerName = "bob";
+function customerName() {
+  return customerName;
+}
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+console.log(upperCaseCustomerName());
+
+function setBestCustomer() {
+  return customerName;
+}
+function overwriteBestCustomer() {
+  const newName = (customerName = "John");
+  return newName;
+}
+console.log(overwriteBestCustomer());
+
+function changeLeastFavoriteCustomer() {
+  return newName;
+}
+console.log(changeLeastFavoriteCustomer());
